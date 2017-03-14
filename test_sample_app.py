8d700f897b829d16f027bda0efd5e0ef43f4a375
RM <rangers1500@yahoo.com>
2017-03-14 13:03:04 -0700

    Rename and sample app updates

--- test_sample_app.py
+++ test_sample_app.py
@@ -1,8 +1,5 @@
 ﻿
 import sys
-import os
-import re
-import traceback
 
 """
 #Normal automatic instantiation
@@ -16,10 +13,9 @@ from trakerr import TrakerrHandler
 """
 
 
-#Without handler custom peramiters
+# Without handler custom peramiters
 from trakerr import TrakerrClient
 from trakerr_client.models import CustomData, CustomStringData
-
 
 
 def main(argv=None):
@@ -36,7 +32,6 @@ def main(argv=None):
     except:
         logger.exception("Bad math.")
     """
-
 
     """
     #Manual instantiation of the logger.
@@ -56,26 +51,31 @@ def main(argv=None):
     try:
         raise IndexError("Bad Math")
     except:
-        appevent = client.create_new_app_event_error("ERROR", "Index Error", "Math")
-        #appevent = client.create_new_app_event("ERROR", "Index Error", "Math") #You can use this call to create an app event without a stacktrace,
-        #in case you do don't have a stacktrace or you're not sending a crash.
+        appevent = client.create_new_app_event("ERROR", "Index Error", "Math")
+        #appevent = client.create_new_app_event("ERROR", "Index Error", "Math")
+        #  #You can use this call to create an app event without a stacktrace,
+        # in case you do don't have a stacktrace or you're not sending a crash.
 
-        #Populate any field with your own data, or send your own custom data
+        # Populate any field with your own data, or send your own custom data
         appevent.context_app_os = "Windows 8"
+        # Can support multiple ways to input data
         appevent.custom_properties = CustomData("Custom Data holder!")
-        appevent.custom_properties.string_data = CustomStringData("Custom String Data 1", "Custom String Data 2") #Can support multiple data
+        appevent.custom_properties.string_data = CustomStringData("Custom String Data 1",
+                                                                  "Custom String Data 2")
         appevent.custom_properties.string_data.custom_data3 = "More Custom Data!"
         appevent.event_user = "john@traker.io"
         appevent.event_session = "6"
 
-        #send it to trakerr
+        # send it to trakerr
         client.send_event_async(appevent)
-    
+
     return 0
+
 
 def error():
     raise EOFError()
 
+
 if __name__ == "__main__":
-    #main()
+    # main()
     sys.exit(main())
